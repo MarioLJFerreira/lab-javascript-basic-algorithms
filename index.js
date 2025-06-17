5e8f4653149926a9460503344fd9f0965d992d98
Mario Ferreira <marioferreira0212@gmail.com>
2025-06-17 15:37:41 +0100

    done final

--- index.js
+++ index.js
@@ -11,10 +11,29 @@ if (hacker1.length > hacker2.length){
 } else if(hacker1.length == hacker2.length){
     console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
 } else {
-    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
 }
 
 // Iteration 3: Loops
+let driverNameSeperated = '';
 for (let i = 0; i < hacker1.length; i++){
-    
+    driverNameSeperated += `${hacker1[i].toUpperCase()} `;
+}
+
+console.log(driverNameSeperated);
+
+let navigatorNameReverse = '';
+for (let i = hacker2.length-1; i >=0; i--){
+    navigatorNameReverse += hacker2[i];
+}
+
+console.log(navigatorNameReverse);
+
+
+if (hacker1.localeCompare(hacker2)<0){
+    console.log(`The driver's name goes first.`);
+} else if(hacker1.localeCompare(hacker2)>0){
+    console.log(`Yo, the navigator goes first, definitely.`);
+} else {
+    console.log(`What?! You both have the same name?`);
 }
